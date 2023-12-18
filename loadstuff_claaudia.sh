9eddb3c9c63349df39971497b97297f755b0bc7b
UR36RV <ur36rv@bio.aau.dk@axomamma.srv.aau.dk>
2023-12-18 10:32:48 +0100

    updated conda env

--- loadstuff_claaudia.sh
+++ loadstuff_claaudia.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 module load Mamba/4.14.0-0
-source activate /user_data/rhk/_conda_envs/2023-01-basecalling-benchmarks
+source activate /home/bio.aau.dk/ur36rv/.conda/envs/2023-01-basecalling-benchmarks
 pip install -r pip_requirements.txt
